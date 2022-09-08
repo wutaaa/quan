a00ff2fb57cd7f221ccf613f2f3e767af03fa27d
chwuta <95623697+chwuta@users.noreply.github.com>
2022-09-08 22:02:45 +0800

    Update Fileball.js

--- Fileball.js
+++ Fileball.js
@@ -7,7 +7,7 @@ hostname= api.revenuecat.com
 
 
 # Fileball 解锁高级版
-^https?:\/\/api\.revenuecat\.com\/v\d\/(subscribers/\$RCAnonymousID\%(\w)+|receipts)$ url echo-response text/Json echo-response Fileball.js
+^https?:\/\/api\.revenuecat\.com\/v\d\/(subscribers/\$RCAnonymousID\%(\w)+|receipts)$ url echo-response text/Json echo-response https://raw.githubusercontent.com/chwuta/quan/main/Fileball.js
 
 
 {
