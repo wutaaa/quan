77a85eba70f5bcf7e4dae9a8aacec7ec38afb1e0
wutaaa <95623697+wutaaa@users.noreply.github.com>
2022-09-09 00:25:45 +0800

    Update wpsdocer.js

--- wpsdocer.js
+++ wpsdocer.js
@@ -11,7 +11,7 @@
 *******************************
 [rewrite_local]
 # > WPS解锁稻壳会员
-^https?:\/\/.+\.(docer.)?wps.cn\/(user\/v1\/vip|android\/mb\/buy|download\/v1\/ios|partner\/invoke\/usable|(api|rank)\/v1(\/mobile\/mb)?\/detail) url script-request-header https://raw.githubusercontent.com/yqc007/QuantumultX/master/WPSDocerVIPowerCrack.js
+^https?:\/\/.+\.(docer.)?wps.cn\/(user\/v1\/vip|android\/mb\/buy|download\/v1\/ios|partner\/invoke\/usable|(api|rank)\/v1(\/mobile\/mb)?\/detail) url script-request-header https://raw.githubusercontent.com/wutaaa/quan/main/wpsdocer.js
 [mitm] 
 hostname = *.docer.wps.cn, vipapi.wps.cn
 *******************************/
